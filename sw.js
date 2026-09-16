49d4c194fc06b54632d6898c3db07165ac71ab19
domxx1 <43669934+domxx1@users.noreply.github.com>
2026-09-16 22:16:02 +0200

    Bump PWA cache for v0.7.25

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="wow-charplan-v46";
+const CACHE="wow-charplan-v47";
 const ASSETS=[
   "./","./index.html","./app.css","./bundle.js","./version.json","./manifest.webmanifest",
   "./icons/icon-192.png","./icons/icon-512.png","./icons/apple-touch-icon.png"
